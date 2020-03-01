1c4c75b2d1a95718ac652a62de379a33c7878666
tanakasotaro <strunv0302@gmail.com>
2020-03-01 19:01:53 +0900

    First

---
+++ RPG/Assets/Enemy_move.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_move : MonoBehaviour
+{
+    private int framecnt=0;
+    private int framecntend;
+    private int scjudge=1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 pos = this.gameObject.transform.position;
+        int xn = Random.Range(-20,20);
+        int yn = Random.Range(-20,20);
+		this.gameObject.transform.position = new Vector3 (pos.x + xn, pos.y, pos.z + yn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(framecnt==0){
+            framecntend=60*Random.Range(1,4);
+        }
+
+        if(framecnt==framecntend){
+            framecnt=0;
+            scjudge*=-1;
+            Debug.Log("change");
+        }
+
+        if(scjudge==1){
+            transform.position += transform.forward * 0.025f;
+            Debug.Log("strate");
+        }else{
+            transform.Rotate(0,1,0);
+            Debug.Log("turn");
+        }
+
+        framecnt++;
+    }
+
+}
---
+++ RPG/Assets/Player_move.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player_move : MonoBehaviour
+{
+
+    public int moveflag = 0;
+    public Text texts;
+    public GameObject panels;
+    public GameObject player;
+    public GameObject king;
+    public GameObject In1; 
+    public GameObject In2; 
+    public GameObject Out;
+    public GameObject enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FadeManager.FadeIn()；
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moveflag==0){
+            if(Input.GetKey("up")){
+                transform.position += transform.forward * 0.2f;
+            }
+
+            if(Input.GetKey("down")){
+                transform.position -= transform.forward * 0.2f;
+            }
+
+            if(Input.GetKey("right")){
+                transform.Rotate(0,5,0);
+            }
+
+            if(Input.GetKey("left")){
+                transform.Rotate(0,-5,0);
+            }
+            panels.SetActive(false);
+            texts.enabled=false;
+        }else{
+            if(moveflag==1){
+                texts.text="部屋に入りますか？";
+                panels.SetActive(true);
+                texts.enabled=true;
+                if(Input.GetKey(KeyCode.Y)){
+                    SceneManager.LoadScene("KingScene");
+                }
+
+                if(Input.GetKey(KeyCode.N)){
+                    moveflag=0;
+                }
+
+            }
+
+            if(moveflag==2){
+                texts.text="外の敵を3体倒してボスに向かえ";
+                panels.SetActive(true);
+                texts.enabled=true;
+                if(Input.GetKey(KeyCode.Y)){
+                    moveflag=0;
+                }
+
+            }
+
+            if(moveflag==3){
+                texts.text="外に出ますか?";
+                panels.SetActive(true);
+                texts.enabled=true;
+                if(Input.GetKey(KeyCode.Y)){
+                    SceneManager.LoadScene("SampleScene");
+                }
+                if(Input.GetKey(KeyCode.N)){
+                    moveflag=0;
+                }
+
+            }
+
+        }
+        
+    }
+
+    void OnCollisionEnter(Collision obj){
+        if(obj.gameObject.tag=="in"){
+            moveflag=1;
+        }
+
+        if(obj.gameObject.tag=="king"){
+            moveflag=2;
+        }
+
+        if(obj.gameObject.tag=="out"){
+            moveflag=3;
+        }
+
+        if(obj.gameObject.tag=="enemy"){
+            //moveflag=4;
+            SceneManager.LoadScene("BattleScene");
+        }
+
+    }
+}
